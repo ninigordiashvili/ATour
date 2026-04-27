cdc936a03238ce8e6e3e838944264f1f05ddde18
Nini Gordiashvili <gordiashvilinini@gmail.com>
2026-04-27 12:55:54 +0400

    last edits

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-import type { NextConfig } from "next";
-
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
-const nextConfig: NextConfig = withNextIntl({
+const nextConfig = withNextIntl({
   reactStrictMode: true,
   compiler: {
     styledComponents: {
